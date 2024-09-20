13e43113b0c6d2526e496df549eed30f107174ed
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-20 13:57:24 +0200

    Bonussheet 4.3.2 (ShoppingList) solved

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,30 @@ class S4536 extends StatelessWidget {
   const S4536({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.shopping_cart),
+          title: Text("${products[index].name} ${products[index].price}"),
+        );
+      },
+      itemCount: products.length,
+      shrinkWrap: true,
+    );
   }
+}
+
+List<Product> products = [
+  Product(name: "Apfel", price: 4.99),
+  Product(name: "Birne", price: 7.99),
+  Product(name: "Wassermelone", price: 19.99),
+  Product(name: "Banane", price: 15.99),
+  Product(name: "Käsebrot", price: 99.99),
+];
+
+class Product {
+  String name;
+  double price;
+
+  Product({required this.name, required this.price});
 }
