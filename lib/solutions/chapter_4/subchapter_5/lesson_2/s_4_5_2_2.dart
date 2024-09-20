681e6fc84dac6be69de1048f2b8fcb47e95c1267
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-20 12:49:20 +0200

    Tasksheet 4.3.2 solved

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,21 @@ class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text("Hello Sascha"),
+        ElevatedButton(onPressed: null, child: Text("Klick mich"))
+      ],
+    )
+    ;
   }
 }
