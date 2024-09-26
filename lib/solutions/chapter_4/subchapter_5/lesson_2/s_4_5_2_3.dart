907b4ba5d666b82a68f4555aca7c56d653c24132
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-26 11:31:49 +0200

    4.5.2.3 solved

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
-class S4523 extends StatelessWidget {
+class S4523 extends StatefulWidget {
   const S4523({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => MyNameWidget();
+}
+
+class MyNameWidget extends State<S4523> {
+  String name = "";
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(
+          onPressed: () {
+            name = "Sascha";
+            setState(() {});
+          },
+          child: const Text("Name anzeigen"),
+        )
+      ],
+    );
   }
 }
