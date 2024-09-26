1241d05440b1c8a6b103af75eba69e2ef08e39c4
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-26 17:02:03 +0200

    4.5.2.4 solved

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
 
-class S4524 extends StatelessWidget {
+class S4524 extends StatefulWidget {
   const S4524({Key? key}) : super(key: key);
   @override
+  State<S4524> createState() => ShowHideNameWidget();
+}
+
+class ShowHideNameWidget extends State<S4524> {
+  String nameHide = "";
+  String nameButton = "Name anzeigen";
+  bool isButtonpressed = false;
+
+  void showHideButton() {
+    if (nameHide == "") {
+      nameHide = "Sascha";
+      nameButton = "Name verstecken";
+    } else {
+      nameHide = "";
+      nameButton = "Name anzeigen";
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Text(nameHide),
+        ElevatedButton(
+          onPressed: showHideButton,
+          child: Text(nameButton),
+        )
+      ],
+    );
   }
 }
